ede859b3f52f4959db04249b18b32f154ad00789
Ramaruva <ramaruva54@gmail.com>
2023-08-25 13:35:20 -0500

    first react app

---
+++ my_frist_react_app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let name= "Pranita"
+  return (
+    <div className="App">
+     <h1>
+      Hello React
+           {name}
+     </h1>
+    </div>
+  );
+}
+
+export default App;

9fa19e65e4ebb3a55fa5cca1f091222fce6c1978
Ramaruva <ramaruva54@gmail.com>
2023-09-10 22:11:53 -0500

    react pract1

--- my_frist_react_app/src/Components/Mybutton.jsx
+++ my_frist_react_app/src/Components/Mybutton.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Mybutton = () => {
+  const [count,setCount] = useState(0);
+  const products =[
+    { title: 'Cabbage', id: 1 },
+    { title: 'Garlic', id: 2 },
+    { title: 'Apple', id: 3 },
+  ]
+  const listItems = products.map(product=>
+    <li key={product.id}>
+       {product.title}
+    </li>
+  );
+  const handleclick= (e)=>{
+     console.log(e)
+     setCount(count+1);
+  }
   return (
-    <button>
-      I am button
-    </button>
+    <>
+    hllo
+    <ul >{listItems}</ul>
+    <button onClick={handleclick}>clicked {count}</button>
+    </>
   )
 }
 
